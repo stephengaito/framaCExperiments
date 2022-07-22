737f2f824883bfd54ada2976f5dfe523a2538714
Stephen Gaito <stephen@perceptisys.co.uk>
2022-07-22 06:33:59 +0100

    continued experiments

--- dynAlloc/main.c
+++ dynAlloc/main.c
@@ -7,20 +7,29 @@ typedef struct complexStruct {
 	struct complexStruct *next;
 } ComplexObj;
 
+/*@
+  requires \valid(anObj);
+ */
+void initObj(ComplexObj *anObj) {
+	anObj->val = 42;
+	anObj->prev = anObj;
+	anObj->next = anObj;
+}
+
 int main(void) {
 
   // We start with some simple arrays of int's and double's
-  int *intArray = calloc(10, sizeof(int) );
-  if (!intArray) return 1;
+//  int *intArray = calloc(10, sizeof(int) );
+//  if (!intArray) return 1;
   // NOTE the following assertion FAILS if we do not ensure intArray is
   // not NULL!
-  //@ assert \valid(intArray);
-  for (int i = 0 ; i < 10; i++) intArray[i] = i;
+  // @ assert \valid(intArray);
+//  for (int i = 0 ; i < 10; i++) intArray[i] = i;
 
-  double *dblArray = calloc(10, sizeof(double));
-  if (!dblArray) return 2;
-  //@ assert \valid(dblArray);
-  for (int i = 0 ; i < 10; i++) dblArray[i] = (double)i;
+//  double *dblArray = calloc(10, sizeof(double));
+//  if (!dblArray) return 2;
+  // @ assert \valid(dblArray);
+//  for (int i = 0 ; i < 10; i++) dblArray[i] = (double)i;
 
 
   // Now we calloc an array of complex structures
@@ -32,17 +41,17 @@ int main(void) {
 
   // Now we calloc an array of pointers to complex structures
 
-  ComplexObj **cObjPtrArray = (ComplexObj**)calloc(10, sizeof(ComplexObj*));
-  if (!cObjPtrArray) return 4;
-  //@ assert \valid(cObjPtrArray);
-  for (int i = 0; i < 10; i++) {
-  	cObjPtrArray[i] = calloc(1, sizeof(ComplexObj));
-  	if (!cObjPtrArray[i]) return 5;
-  	//@ assert \valid(cObjPtrArray[i]);
-  }
+//  ComplexObj **cObjPtrArray = (ComplexObj**)calloc(10, sizeof(ComplexObj*));
+//  if (!cObjPtrArray) return 4;
+  // @ assert \valid(cObjPtrArray);
+//  for (int i = 0; i < 10; i++) {
+//  	cObjPtrArray[i] = calloc(1, sizeof(ComplexObj));
+//  	if (!cObjPtrArray[i]) return 5;
+//  	//@ assert \valid(cObjPtrArray[i]);
+//  }
 
-  for (int i = 0; i < 9;  i++) cObjPtrArray[i]->next = cObjPtrArray[i+1];
-  for (int i = 1; i < 10; i++) cObjPtrArray[i]->prev = cObjPtrArray[i-1];
+//  for (int i = 0; i < 9;  i++) cObjPtrArray[i]->next = cObjPtrArray[i+1];
+//  for (int i = 1; i < 10; i++) cObjPtrArray[i]->prev = cObjPtrArray[i-1];
 
 	return 0;
 }
